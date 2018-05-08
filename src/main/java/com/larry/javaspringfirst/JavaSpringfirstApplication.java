476c6b41e73f29ab5406eb91053421fcc3940f6b
larryjung <corbswjd@gmail.com>
2018-05-08 17:53:51 +0900

    init

---
+++ src/main/java/com/larry/javaspringfirst/JavaSpringfirstApplication.java
@@ -0,0 +1,12 @@
+package com.larry.javaspringfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringfirstApplication.class, args);
+	}
+}
